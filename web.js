6238091b6cabaca7109241d5bdbed23d6e6fd1a7
Wenbo Tan <tanwenbo@gmail.com>
2013-07-20 06:21:15 +0000

    Read data from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+
+var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    var fileData = fs.readFile('index.html').toString();
+
+    response.send(fileData);
 });
 
 var port = process.env.PORT || 5000;
